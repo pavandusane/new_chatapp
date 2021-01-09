d4772751fe29f43aac59b1aac4456a07336bafbe
pavandusane <61798553+pavandusane@users.noreply.github.com>
2021-01-09 12:08:27 +0530

    Update afterinstallation.sh

--- afterinstallation.sh
+++ afterinstallation.sh
@@ -4,5 +4,5 @@ sudo systemctl daemon-reload
 sudo systemctl enable gunicorn
 sudo ln -s /etc/nginx/sites-available/fundoo /etc/nginx/sites-enabled
 sudo nginx -t
-sudo rm /etc/nginx/sites-enabled/default
+
 

9a8b93f593f99c656db28ca1d699d6812469bdd5
pavandusane <61798553+pavandusane@users.noreply.github.com>
2021-01-10 22:11:16 +0530

    Update afterinstallation.sh

--- afterinstallation.sh
+++ afterinstallation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+gunicorn --bind 0.0.0.0:8000 fundoo.wsgi:application
 sudo systemctl daemon-reload
 sudo systemctl enable gunicorn
 sudo ln -s /etc/nginx/sites-available/fundoo /etc/nginx/sites-enabled
 sudo nginx -t
-
+sudo service nginx restart
 

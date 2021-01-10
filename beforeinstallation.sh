dad3e5a4cefb93a5a351da0ee7f289740ff2116f
pavandusane <61798553+pavandusane@users.noreply.github.com>
2021-01-10 22:41:53 +0530

    Update beforeinstallation.sh

--- beforeinstallation.sh
+++ beforeinstallation.sh
@@ -4,9 +4,9 @@ sudo apt-get update
 sudo apt-get install python3-pip python-dev nginx git
 sudo apt-get update
 sudo pip3 install virtualenv
+cd new_chatapp
 virtualenv venv
-source venv/bin/activate
-cd new_chatapp																
+source venv/bin/activate																
 pip3 install -r requirements.txt
 pip3 install django bcrypt django-extensions
 pip3 install gunicorn
